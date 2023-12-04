5795ff288eb59483d161394d7c285f1ca4dbad16
Melissa <melissamwaltner@gmail.com>
2023-12-04 13:19:24 -0600

    Setup for vite and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./templates/index.html",
+    './inc/**/*.php',
+    // "./assets/main.css",
+    // "./main.js",
+    // "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
