7c49e94485a2566afbed8e8dc4ca1bdbc2c10b17
Melissa <melissamwaltner@gmail.com>
2023-12-04 15:21:25 -0600

    Updates to utilize blocks and compatability for vite

---
+++ blocks/register-blocks.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * We use WordPress's init hook to make sure
+ * our blocks are registered early in the loading
+ * process.
+ *
+ * @link https://developer.wordpress.org/reference/hooks/init/
+ */
+function tt3child_register_acf_blocks() {
+    /**
+     * We register our block's with WordPress's handy
+     * register_block_type();
+     *
+     * @link https://developer.wordpress.org/reference/functions/register_block_type/
+     */
+    register_block_type( __DIR__ . '/team-member' );
+}
+// Here we call our tt3child_register_acf_block() function on init.
+add_action( 'init', 'tt3child_register_acf_blocks' );
